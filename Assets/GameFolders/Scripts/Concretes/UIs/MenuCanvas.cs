f3e8e5e73fd40222175a3705235cb08417c3485c
mehmetimga <imgamehmet@gmail.com>
2023-05-25 20:16:41 +0300

    66-menu scene 1

---
+++ Assets/GameFolders/Scripts/Concretes/UIs/MenuCanvas.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UdemyBerkProject1.UIs
+{
+    public class MenuCanvas : MonoBehaviour
+    {
+        public void ExitButtonClick()
+        {
+
+        }
+        public void StartButtonClick()
+        {
+
+        }
+    }
+}
+
